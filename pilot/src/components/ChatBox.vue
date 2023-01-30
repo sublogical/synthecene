22b4f10698498d2bc890c1fc58d5b9d23c63fa8a
Jay Hoover <sublogical@gmail.com>
2023-01-29 22:48:18 -0800

    playing with design

--- pilot/src/components/ChatBox.vue
+++ pilot/src/components/ChatBox.vue
@@ -1,14 +1,17 @@
 <template>
     <div id="ChatBox">
+        <div class="ChatBox__List">
+            <chat-bubble v-for="message in messages" :data="message"></chat-bubble>
+        </div>
+    </div>
+
+    <div id="Footer">
         <div class="ChatBox__Input">
             <v-text-field
                 hide-details
                 prepend-icon="mdi-magnify"
                 single-line
             ></v-text-field>
-        </div>
-        <div class="ChatBox__List">
-            <chat-bubble v-for="message in messages" :data="message"></chat-bubble>
         </div>
     </div>
 </template>
@@ -19,11 +22,130 @@
         components: { ChatBubble },
         data() {
             return {
-                messages: [{
-                    text: "Hi, how can I help you?",
-                    agent: true
-                }],
+                messages: [
+                    {
+                        text: "Hi, how can I help you?",
+                        agent: true
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'm sorry, we don't have any giraffes available at the moment. Would you like to order a 60' tall giraffe instead?",
+                        agent: true
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                    {
+                        text: "I'd like to order a 80' tall giraffe. How much is that?",
+                        agent: false
+                    },
+                ],
             }
         }
     }
 </script>
+
+<style>
+#ChatBox {
+    display: flex;
+    flex-direction: column;
+    width: 800px;
+    height: 100%;
+}
+#Footer{
+ position:fixed;
+ bottom:0;
+ width: 800px;
+ z-index: 100;
+ opacity: 1;
+ background-color: rgb(var(--v-primary-base));
+}
+</style>
--- pilot/src/components/ChatBubble.vue
+++ pilot/src/components/ChatBubble.vue
@@ -29,11 +29,9 @@
         margin: 0.5rem 0;
     }
     .chat-bubble.agent {
-        text-align: left;        
     }
 
     .chat-bubble.user {
-        text-align: right;
     }
     .chat-bubble__content {
         display: flex;
@@ -42,7 +40,7 @@
         margin: 0.5rem 0;
         padding: 1rem 1rem;
         border-radius: 1rem;
-        background-color: #202020;
+        background-color: #202080;
     }
     .chat-bubble.agent .chat-bubble__content {
         align-items: flex-end;
