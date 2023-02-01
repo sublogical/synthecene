67190412067e799997f32fb94130e6647ccf9395
Jay Hoover <sublogical@gmail.com>
2023-01-31 22:15:22 -0800

    moved to warp

--- dexter/src/main.rs
+++ dexter/src/main.rs
@@ -1,21 +1,26 @@
 use tokio::time::{Duration, sleep};
+use warp::Filter;
 
-#[macro_use] extern crate rocket;
-
-#[get("/world")]
 fn world() -> &'static str {
     "Hello, world!\n"
 }
 
-#[get("/delay/<seconds>")]
-async fn delay(seconds: u64) -> String {
+async fn delay(seconds: u64) -> Result<String, warp::Rejection> {
     sleep(Duration::from_secs(seconds)).await;
-    format!("Waited for {} seconds\n", seconds)
+    Ok(format!("Waited for {} seconds\n", seconds))
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
-        .mount("/hello", routes![world])
-        .mount("/", routes![delay])
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello")
+        .map(world);
+
+    let delay = warp::path!("delay" / u64)
+        .and_then(delay);
+
+    let routes = hello.or(delay);
+
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
