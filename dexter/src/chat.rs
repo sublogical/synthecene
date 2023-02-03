5f0c33037bb3cb280e123c4f5a66e788946d7b69
Jay Hoover <sublogical@gmail.com>
2023-02-02 22:37:47 -0800

    moved to axios and added interfaces, started working on stateful chat

--- dexter/src/chat.rs
+++ dexter/src/chat.rs
@@ -1,6 +1,6 @@
-use std::fmt::Debug;
+use std::{fmt::Debug, collections::{HashMap, hash_map::Entry}, sync::Arc};
 use serde::{Deserialize, Serialize };
-use tokio::sync::mpsc;
+use tokio::sync::{mpsc, RwLock};
 use warp::{Filter};
 
 
@@ -95,37 +95,63 @@ fn delete_chat_route() -> impl Filter<Extract = impl warp::Reply, Error = warp::
 pub struct ChatMessage {
     pub chat_id: String,
     pub message_id: String,
+    pub agent: bool,
     pub message: String,
 }
 
+#[derive(Clone, Debug, Hash, PartialEq, Eq, Deserialize, Serialize)]
+pub struct ChatMessageRequest {
+    pub agent: bool,
+    pub message: String,
+}
 /**
  * POST   /chat/{session}/message - create a message
  */
-async fn create_message(session_id: String) -> Result<impl warp::Reply, warp::Rejection> {
-    let result = ChatMessage { chat_id: session_id, message_id: "1".to_string(), message: "Hello, world!".to_string() };
-    Ok(warp::reply::json(&result))
+async fn create_message(session_id: String, 
+                        request: ChatMessageRequest,
+                        chat_store: ChatStore) -> Result<impl warp::Reply, warp::Rejection> {
+
+    let new_message = ChatMessage { 
+        chat_id: session_id.clone(), 
+        message_id: "1".to_string(),
+        agent: request.agent, 
+        message: request.message
+    };
+
+    {
+        let mut chat_store = chat_store.write().await;
+
+        let mut chat = match chat_store.entry(session_id.clone()) {
+            Entry::Occupied(o) => o.into_mut(),
+            Entry::Vacant(v) => v.insert(vec![]),
+        };
+
+        chat.push(new_message.clone());
+    }
+
+    Ok(warp::reply::json(&new_message))
 }
 
-fn create_message_route() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+fn create_message_route(chat_store: ChatStore) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path!("chat" / String / "message")
         .and(warp::post())
+        .and(warp::body::json())
+        .and(warp::any().map(move || chat_store.clone()))
         .and_then(create_message)
 }
 
 /**
  * GET    /chat/{session}/message - list messages
  */
-async fn list_messages(session_id: String) -> Result<impl warp::Reply, warp::Rejection> {
-    let result = vec![
-        ChatMessage { chat_id: session_id.clone(), message_id: "1".to_string(), message: "Hello, world!".to_string() },
-        ChatMessage { chat_id: session_id.clone(), message_id: "2".to_string(), message: "Hello, world!".to_string() },
-    ];
+async fn list_messages(session_id: String, chat_store: ChatStore) -> Result<impl warp::Reply, warp::Rejection> {
+    let result = chat_store.read().await.get(&session_id).unwrap_or(&vec![]).clone();
     Ok(warp::reply::json(&result))
 }
 
-fn list_messages_route() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+fn list_messages_route(chat_store: ChatStore) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path!("chat" / String / "message")
         .and(warp::get())
+        .and(warp::any().map(move || chat_store.clone()))
         .and_then(list_messages)
 }
 
@@ -133,7 +159,7 @@ fn list_messages_route() -> impl Filter<Extract = impl warp::Reply, Error = warp
  * GET    /chat/{session}/message/{message} - get a message
  */
 async fn get_message(session_id: String, message_id: String) -> Result<impl warp::Reply, warp::Rejection> {
-    let result = ChatMessage { chat_id: "1".to_string(), message_id: "1".to_string(), message: "Hello, world!".to_string() };
+    let result = ChatMessage { chat_id: "1".to_string(), message_id: "1".to_string(), agent: true, message: "Hello, world!".to_string() };
     Ok(warp::reply::json(&result))
 }
 
@@ -147,7 +173,7 @@ fn get_message_route() -> impl Filter<Extract = impl warp::Reply, Error = warp::
  * DELETE /chat/{session}/message/{message} - delete a message
  */
 async fn delete_message(session_id: String, message_id: String) -> Result<impl warp::Reply, warp::Rejection> {
-    let result = ChatMessage { chat_id: "1".to_string(), message_id: "1".to_string(), message: "Hello, world!".to_string() };
+    let result = ChatMessage { chat_id: "1".to_string(), message_id: "1".to_string(), agent: true, message: "Hello, world!".to_string() };
     Ok(warp::reply::json(&result))
 }
 
@@ -156,6 +182,8 @@ fn delete_message_route() -> impl Filter<Extract = impl warp::Reply, Error = war
         .and(warp::delete())
         .and_then(delete_message)
 }
+
+type ChatStore = Arc<RwLock<HashMap<String, Vec<ChatMessage>>>>;
 
 /**
  * POST   /chat - create new session
@@ -168,6 +196,11 @@ fn delete_message_route() -> impl Filter<Extract = impl warp::Reply, Error = war
  * DELETE /chat/{session}/message/{message} - delete a message
  */
 pub(crate) fn routes() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    let chats: ChatStore = Arc::new(RwLock::new(HashMap::new()));
+
     list_chats_route().or(create_chat_route()).or(get_chat_route()).or(delete_chat_route())
-        .or(create_message_route()).or(list_messages_route()).or(get_message_route()).or(delete_message_route())
+        .or(create_message_route(chats.clone()))
+        .or(list_messages_route(chats.clone()))
+        .or(get_message_route())
+        .or(delete_message_route())
 }
--- pilot/src/components/ChatBox.vue
+++ pilot/src/components/ChatBox.vue
@@ -17,6 +17,41 @@
 </template>
 
 <script lang="ts">
+    import axios from 'axios';
+
+    interface Message {
+        chat_id: string,
+        message_id: string,
+        agent: boolean,
+        message: string
+    }
+
+    const hostname = location.hostname;
+    const api_port = 3030;
+
+    function GetMessages(chat_id: string): Promise<Array<Message>> {
+        return axios.get<Array<Message>>(`http://${hostname}:${api_port}/chat/${chat_id}/message`)
+            .then((response: { data: Message; }) => {
+                console.log("GET Response")
+                console.log(response.data);
+                
+                return response.data;
+            });
+    }
+
+    function PostMessage(chat_id: string, message: string) {
+        return axios.post(`http://${hostname}:${api_port}/chat/${chat_id}/message`, {
+            message: message
+        })
+            .then((response: { data: Message; }) => {
+                console.log("POST Response")
+                console.log(response.data);
+                
+                return response.data;
+            });
+    }
+
+
     import ChatBubble from './ChatBubble.vue'
     import { ref, computed, onMounted } from "vue";
     import type { Ref } from 'vue'
@@ -32,29 +67,21 @@
             const error: Ref<Error | null> = ref(null);
 
             const api_port = 3030;
-            const chat_id = 1;
+            const chat_id = "1";
 
             const chat_url = `http://${hostname}:${api_port}/chat/${chat_id}/message`
             function fetchData() {
                 loading.value = true;
 
-                return fetch(chat_url, {
-                    method: 'GET',
-                    headers: {
-                    'content-type': 'application/json'
-                    }
-                }).then(res => {
-                    console.log("got result " + res.status);
-                    if (!res.ok) {
-                    error.value = new Error(res.statusText);;
-                    } else {
-                    res.text().then((str) => {
-                        console.log("got data " + str);
-                        messages.value = JSON.parse(str);
+                GetMessages(chat_id)
+                    .then((data: Array<Message>) => {
+                        console.log("got data " + data);
+                        messages.value = data;
                         loading.value = false;
+                    })
+                    .catch((error: any) => {
+                        error.value = error;
                     });
-                    }
-                })
             }
 
             onMounted(() => {
@@ -82,7 +109,7 @@
  position:fixed;
  bottom:0;
  width: 800px;
- z-index: 100;
+ z-index: 100;  
  opacity: 1;
  background-color: rgb(var(--v-primary-base));
 }
