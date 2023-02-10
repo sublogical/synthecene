7cc1dcdb09c0d6cf7db09c33e6e9819226f9c6af
Jay Hoover <sublogical@gmail.com>
2023-02-10 08:31:41 -0800

    checkpoint in actor system - dont work

--- dexter/src/dexter/mod.rs
+++ dexter/src/dexter/mod.rs
@@ -1,29 +1,24 @@
-use std::{ marker::PhantomData, mem::replace};
+use std::{ marker::PhantomData, mem::replace, sync::RwLock};
 
 use async_trait::async_trait;
 use tokio::sync::{mpsc, oneshot};
+use std::future::Future;
 
 #[derive(Debug)]
 enum Error {
     NoBehavior,
 }
 
-trait Message {
-    type Response;
+struct Behavior<T, F>
+where F: FnOnce(T) -> Behavior<T, F> + Send + 'static
+{
+    proc: Box<dyn FnOnce(T) -> Behavior<T, F>>,
 }
 
-struct MsgRef<T: Message> {
-    content: T,
-    respond_to: ActorHandle<T::Response>,
-}
-
-struct Behavior<T: Message> {
-    proc: Box<dyn FnOnce(MsgRef<T>) -> Behavior<T>>,
-}
-
-impl <T: Message> Behavior<T> {
-    fn new<F>(proc: F) -> Behavior<T> 
-    where F: FnOnce(MsgRef<T>) -> Behavior<T> + 'static
+impl <T,F> Behavior<T,F> 
+where F: FnOnce(T) -> Behavior<T, F> + Send + 'static
+{
+    fn new(proc: F) -> Behavior<T, F> 
     {
         Behavior {
             proc: Box::new(proc),
@@ -33,43 +28,33 @@ impl <T: Message> Behavior<T> {
 
 enum SettableMsg<T> {
     Set(T),
-    Get,
+    Get(ActorRef<T>),
 }
-
-impl <T> Message for SettableMsg<T> 
-where T:
-{
-    type Response = SettableResponse<T>;
-}
-
-
-// todo: find a way to have a generic response type conditioned on the message type not the enum
-enum SettableResponse<T> {
-    Value(T),
-    Ok,
-}
-
-impl <T> Message for SettableResponse<T> 
-{
-    type Response = ();
-}
-
 
 pub struct SettableNode;
 
 impl SettableNode {
-    fn apply<T: Clone + 'static>() -> Behavior<SettableMsg<T>> { Self::empty(vec![]) }
+    fn apply<O, F>() -> Behavior<SettableMsg<O>,F> 
+    where
+        O: Clone + Send + 'static,
+        F: FnOnce(SettableMsg<O>) -> Behavior<SettableMsg<O>, F> + Send + 'static,
+    {
+         Self::empty(vec![])
+    }
 
-    fn empty<T: Clone + 'static>(mut pending: Vec<MsgRef<SettableMsg<T>>>) -> Behavior<SettableMsg<T>> {
-        Behavior::new(move |msg| {
-            match msg.content {
+    fn empty<O, F>(mut pending: Vec<ActorRef<O>>) -> Behavior<SettableMsg<O>,F> 
+    where
+        O: Clone + Send + 'static,
+        F: FnOnce(SettableMsg<O>) -> Behavior<SettableMsg<O>, F> + Send + 'static,
+    {
+        Behavior::new(move |msg:SettableMsg<O>| {
+            match msg {
                 SettableMsg::Set(value) => {
                     Self::notify_pending(&pending, &value);
-                    msg.sender.send(SettableResponse::Ok);
                     Self::resolved(value)
                 }
-                SettableMsg::Get => {
-                    pending.push(msg);
+                SettableMsg::Get(respond_to) => {
+                    pending.push(respond_to);
 
                     Self::empty(pending)
                 }
@@ -77,22 +62,25 @@ impl SettableNode {
         })
     }
 
-    fn notify_pending<T: Clone>(pending: &Vec<MsgRef<SettableMsg<T>>>, value: &T) {
-        for msg in pending {
-            msg.sender.send(SettableResponse::Value(value.clone()));
+    fn notify_pending<T: Clone + Send>(pending: &Vec<ActorRef<T>>, value: &T) {
+        for respond_to in pending {
+            respond_to.tell(value.clone());
         }
         
     }
 
-    fn resolved<T: Clone + 'static>(value: T) -> Behavior<SettableMsg<T>> {
-        Behavior::new(move |msg| {
-            match msg.content {
+    fn resolved<O, F>(value: O) -> Behavior<SettableMsg<O>, F> 
+    where
+        O: Clone + Send + 'static,
+        F: FnOnce(SettableMsg<O>) -> Behavior<SettableMsg<O>, F> + Send + 'static,
+    {
+        Behavior::new(move |msg:SettableMsg<O>| {
+            match msg {
                 SettableMsg::Set(new_value) => {
-                    msg.sender.send(SettableResponse::Value(value));
                     Self::resolved(new_value)
                 }
-                SettableMsg::Get => {
-                    msg.sender.send(SettableResponse::Value(value.clone()));
+                SettableMsg::Get(respond_to) => {
+                    respond_to.tell(value.clone());
                     Self::resolved(value)
                 }
             }
@@ -100,29 +88,44 @@ impl SettableNode {
     }
 }
 
-struct ActorSystem<T: Message> {
-    guardian: Behavior<T>,
+struct ActorSystem<T> {
+    guardian: ActorRef<T>,
 }
 
-impl <T: Message> ActorSystem<T> {
-    fn new(guardian: Behavior<T>, name: &str) -> ActorSystem<T> {
+impl <T:Send> ActorSystem<T> {
+    fn new<F>(guardian: Behavior<T,F>, name: &str) -> ActorSystem<T> 
+    where
+        F: FnOnce(T) -> Behavior<T, F> + Send + 'static,
+    {
+        let guardian = ActorRef::from_behavior(guardian);
         ActorSystem {
             guardian,
         }
     }
+    pub async fn tell(&mut self, msg: T) {
+        let _ = self.guardian.tell(msg).await;
+    }
 
-    async fn send(&self, msg: T) -> MsgRef<T> {
-        todo!()
+    pub async fn ask<R, F>(&self, init: F) -> R 
+    where F: FnOnce(ActorRef<R>) -> T
+    {
+        self.guardian.ask(init).await
     }
 }
 
 
-struct Actor<T: Message> {
-    receiver: mpsc::Receiver<MsgRef<T>>,
-    behavior: Result<Behavior<T>, Error>
+struct Actor<T, F>
+where
+    F: FnOnce(T) -> Behavior<T, F> + Send + 'static
+{
+    receiver: mpsc::Receiver<T>,
+    behavior: Result<Behavior<T, F>, Error>
 }
 
-impl <T: Message> Actor<T> {
+impl <T, F> Actor<T, F>
+where
+    F: FnOnce(T) -> Behavior<T, F> + Send + 'static
+{
     async fn run(&mut self) {
         while let Some(msg) = self.receiver.recv().await {
             if self.behavior.is_err() {
@@ -137,53 +140,72 @@ impl <T: Message> Actor<T> {
 
 #[derive(Clone)]
 pub struct ActorRef<T> {
-    sender: mpsc::Sender<MsgRef<T>>,
+    sender: mpsc::Sender<T>,
 }
 
-impl <T: Message> ActorHandle <T>  {
-    pub fn new(behavior: Behavior<T>) -> Self {
+impl <T: Send> ActorRef <T>  {
+    pub fn from_behavior<F>(behavior: Behavior<T, F>) -> Self 
+    where
+        F: FnOnce(T) -> Behavior<T, F> + Send + 'static,
+    {
         let (sender, receiver) = mpsc::channel(8);
-        let actor = Actor {
+        let mut actor = Actor {
             receiver,
             behavior: Ok(behavior)
         };
 
-        tokio::spawn(Self::run_actor(actor));
+        tokio::spawn(actor.run());
 
         Self { sender }
     }
 
-    async fn run_actor(actor: Actor<T>) {
+    pub fn oneshot(sender: mpsc::Sender<T>) -> Self {
+        Self {
+            sender
+        }
+    }
+    async fn run_actor<F>(mut actor: Actor<T, F>) 
+    where
+        F: FnOnce(T) -> Behavior<T, F> + Send + 'static,
+    {
         actor.run().await;
     }
 
-    pub async fn send(&self, msg: T) -> u32 {
-        let (send, recv) = oneshot::channel();
+    pub async fn tell(&self, msg: T) {
+        let _ = self.sender.send(msg).await;
+    }
 
-        let msg_ref = MsgRef {
-            respond_to: send,
-            content: msg,
-        };
+    pub async fn ask<R, F>(&self, init: F) -> R 
+    where F: FnOnce(ActorRef<R>) -> T
+    {
+        let (sender, mut receiver) = mpsc::channel(8);
+
+        let response_actor = ActorRef { sender };
+
+        let msg = init(response_actor);
 
         // Ignore send errors. If this send fails, so does the
         // recv.await below. There's no reason to check for the
         // same failure twice.
         let _ = self.sender.send(msg).await;
-        recv.await.expect("Actor task has been killed")
+        receiver.recv().await.expect("Actor task has been killed")
     }
 }
+
+use futures::poll;
+use std::task::Poll;
 
 #[tokio::test]
 async fn test_settable_node() {
     let system = ActorSystem::new(
         SettableNode::apply::<i32>(), "settable_node");
 
-    let get_response = system.send(SettableMsg::Get);
-    assert!(get_response.poll().is_pending());
+    let output = Box::pin(system.ask(|actor_ref| {SettableMsg::Get(actor_ref) }));
+    let current = poll!(output);
+    assert_eq!(current, Poll::Pending);
 
-    let set_request = system.send(SettableMsg::Set(42)).await.unwrap();
-    assert!(set_request == SettableResponse::Ok);
+    let set_request = system.tell(SettableMsg::Set(42)).await;
 
-    let get_response = get_response.await.unwrap();
-    assert!(get_response == SettableResponse::Value(42));
+    let current = poll!(output);
+    assert_eq!(current, Poll::Ready(42));
 }
