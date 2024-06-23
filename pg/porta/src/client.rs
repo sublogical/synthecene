374b465a00b2585c74409b12a840e2af1853d872
Jay Hoover <sublogical@gmail.com>
2024-06-23 06:35:44 -0700

    make graph show up right in client

--- pg/porta/src/client.rs
+++ pg/porta/src/client.rs
@@ -9,8 +9,12 @@ pub(crate) mod metrics;
 use metrics::format_metric;
 
 use porta_api::porta_client::PortaClient;
-use porta_api::{MetricRequest, MetricResponse};
-use yansi::{Paint, Style, Color::*};
+use porta_api::MetricRequest;
+use porta_api::number_data_point::Value;
+use textplots::{Chart, ColorPlot, Shape};
+use yansi::Paint;
+
+const RED: rgb::RGB8 = rgb::RGB8::new(0xFF, 0x00, 0x00);
 
 fn cli() -> Command {
     Command::new("portac")
@@ -42,17 +46,37 @@ async fn handle_view(metric_suris:&Vec<&String>) -> Result<(), Box<dyn std::erro
     });
 
     let response = client.get_metrics(request).await?;
-
     let response = response.into_inner();
 
     for metric in response.metric {
         match metric.data {
             Some(porta_api::metric::Data::Gauge(guage)) => {
+                let xy_pairs = guage.data_points.iter().map(|data_point| {
+                    let x = data_point.time_unix_nano as f32 / 1_000_000_000.0;
+                    let y = match data_point.value {
+                        Some(Value::AsDouble(fval)) => (fval as f32).into(),
+                        Some(Value::AsInt(ival)) => (ival as f32).into(),
+                        None => {
+                            panic!("unexpected datapoint with null value");
+                        }
+                    };
+                    (x,y)
+                }).collect::<Vec<(f32, f32)>>();
+
+                let start = xy_pairs.first().unwrap().0;
+                let end = xy_pairs.last().unwrap().0;
+                
+                let lines = Shape::Lines(&xy_pairs);
+                Chart::new(120, 60, start, end)
+                    .linecolorplot(&lines, RED)
+                    .display();
+
                 println!("{}", format_metric(&guage.data_points));
             }
             _ => {}
         }        
     }
+
 
     Ok(())
 }
@@ -70,7 +94,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .flatten()
                 .collect::<Vec<_>>();
 
-            handle_view(&suris).await;
+            // todo: make this pass through the result
+            let _ = handle_view(&suris).await;
         },
         Some((unexpected, _sub_matches)) => {
             println!(
